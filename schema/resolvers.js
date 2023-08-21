a0f5b99ec2a1431cf7a9fbe9a4b29e39f9fb8dad
Aditya Nittala <adityann97@gmail.com>
2023-08-21 15:54:12 -0400

    Added a MovieList JS
    Created the Resolvers for this MovieList

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,15 +1,25 @@
 const UserList = require('./Generateddata')
 const _ = require("lodash");
-
+const MovieList = require('./MovieList');
 const resolvers = {
     Query: {
+        //User Resolver
         users: ()=> {
             return UserList;
         },
         user: (parent,args) =>{
             const id = args.id;
-            const user = _.find(UserList,{id: id});
+            const user = _.find(UserList,{id: Number(id)});
             return user
+        },
+        //Movie Resolvers
+        movies: ()=> {
+            return MovieList
+        },
+        movie: (parent,args)=>{
+            const name = args.name
+            const mov = _.find(MovieList,{name:String(name)})
+            return mov
         }
     }
 }
--- schema/typeDefs.js
+++ schema/typeDefs.js
@@ -8,12 +8,17 @@ const typeDefs = gql `
         age: Int
         nationality: Nationality
         friends: [User] #Fields can be nested inside one another
+        #Let's add another nested type. This time pointing to another file altogether
+
+        favmovie: [Movies]
     }
 
     type Query{
         users: [User!]!
         user(id: ID!): User! #This will allow for the selecting of certain users based on the ID providede. The ID MUST be provided
         #The return type is User.
+        movies: [Movies!]!
+        movie(name: String!): Movies!
     }
 
     enum Nationality{
@@ -22,6 +27,13 @@ const typeDefs = gql `
         Spanish
         Pakistani
         Australian
+    }
+
+    type Movies{
+        id: ID!
+        name: String!
+        releaseyear: Int!
+        isintheatres: Boolean!
     }
 `;
 
