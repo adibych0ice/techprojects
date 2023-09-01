f05582bf3411ef632c8ec9f45657325500f49358
Aditya Nittala <adityann97@gmail.com>
2023-09-01 11:26:36 -0400

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
     <div className="App">
-      <DisplayData / >
+      <DisplayData></DisplayData>
     </div>
     </ApolloProvider>
   );
